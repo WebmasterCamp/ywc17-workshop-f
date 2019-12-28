60733b97958f3e28d4c74a7258926a57e69249ae
Phoomparin Mano <phoomparin@gmail.com>
2019-12-28 14:36:10 +0700

    :sparkles: Add cover photo

--- js/pages/Detail.vue
+++ js/pages/Detail.vue
@@ -3,8 +3,10 @@
     nav-bar
     section.cover-section(:style="image")
       .backdrop-gradient
+        .brief-info
+          img.profile-photo(:src="data.image")
+          .name {{data.title}}
     section.container
-      .school-name {{data.title}}
 </template>
 
 <script>
@@ -25,7 +27,7 @@ export default {
     },
     image() {
       return {
-        'background-image': `url(${this.data.image})`,
+        'background-image': `url(${this.data.coverImage})`,
       }
     },
   },
@@ -33,14 +35,20 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+  @use '../../styles/variables' as *
+
   .container
     margin: 50px 50px
+
+  .name
+    color: white
+    font-size: 32px
 
   .cover-section
     position: relative
     width: 100%
     height: 250px
-    background-color: #C4C4C4
+    background-color: $primary
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1)
     background-size: cover
     background-attachment: scroll
@@ -51,4 +59,16 @@ export default {
     width: 100%
     height: 100%
     background: linear-gradient(0deg, rgba(0, 0, 0, 0.6) 7%, transparent 46%)
+
+    display: flex
+    align-items: flex-end
+    justify-content: center
+    padding-bottom: 17px
+
+  .brief-info
+    display: flex
+
+  .profile-photo
+    width: 150px
+    height: 150px
 </style>
